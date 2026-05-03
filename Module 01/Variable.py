60b6f2f0d65dc7682a2f0f7dd243bbd0ca25b768
MD. Jahidul Islam Jony <jahiduljony60@gmail.com>
2026-05-04 01:40:39 +0600

    Variable.py

--- Module 01/Variable.py
+++ Module 01/Variable.py
@@ -2,29 +2,24 @@ a = 10
 print( a )
 print ( type (a) )
 
-
 b = 10.0
 print ( type (b) )
 
 a = b
 print( a )
-print ( type (a) )
-
+print ( type (a) 
 
 name = "Jony"
 print ( type (name) )
-
 
 data = input()
 print( data )
 print ( type(data) )
 
-
 a = input()
 a = int(a)
 print( a )
 print ( type (a) )
-
 
 row_number = input()
 numbers = row_number.split()
