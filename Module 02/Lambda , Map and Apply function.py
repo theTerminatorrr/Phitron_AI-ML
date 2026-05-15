30dfe5bc9b6cd32fe63c1e6f6a76b1401109b445
MD. Jahidul Islam Jony <jahiduljony60@gmail.com>
2026-05-15 22:26:21 +0600

    Lambda , Map and Apply function.py

--- Module 02/Lambda , Map and Apply function.py
+++ Module 02/Lambda , Map and Apply function.py
@@ -1,13 +1,10 @@
 import pandas as pd
 
-
 df = pd.DataFrame({'temperature (F)': [32, 212, 98.6, 68]})
 df
 
 df['temperature (C)'] = df['temperature (F)'].apply(lambda x: (x - 32) * 5/9)
 df
-
-
 
 import pandas as pd
 
@@ -18,3 +15,5 @@ df
 df['A'] = df['A'].apply(lambda x: x ** 0.5)
 
 df
+
+
